44402656d95a8c0db27e7dc1f3e16ef5eb420cef
Kelvingenao Carmona <genaokelvin138@gmail.com>
2025-01-05 21:35:28 +0000

    fix: adjust script output to meet requirements

--- element.sh
+++ element.sh
@@ -9,19 +9,22 @@ if [[ -z $1 ]]; then
   exit 0
 fi
 
-# Validar si el argumento es válido
-if [[ ! $1 =~ ^[0-9]+$ ]] && [[ ! $1 =~ ^[A-Za-z]+$ ]]; then
-  echo "Please provide a valid atomic number, symbol, or element name."
-  exit 0
+# Consulta para buscar el elemento por número atómico, símbolo o nombre
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+         FROM elements
+         INNER JOIN properties USING(atomic_number)
+         INNER JOIN types USING(type_id)
+         WHERE atomic_number = $1"
+else
+  QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+         FROM elements
+         INNER JOIN properties USING(atomic_number)
+         INNER JOIN types USING(type_id)
+         WHERE symbol = '$1' OR name = '$1'"
 fi
 
-# Consulta para buscar el elemento por número atómico, símbolo o nombre
-QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
-       FROM elements
-       INNER JOIN properties USING(atomic_number)
-       INNER JOIN types USING(type_id)
-       WHERE atomic_number = $1 OR symbol = '$1' OR name = '$1'"
-
+# Ejecutar la consulta
 RESULT=$($PSQL "$QUERY")
 
 # Validar si el resultado de la consulta está vacío
@@ -30,8 +33,6 @@ if [[ -z $RESULT ]]; then
   exit 0
 else
   # Procesar y mostrar los resultados
-  echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING
-  do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
-  done
+  IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
 fi
